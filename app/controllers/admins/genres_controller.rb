8c722a5759af533a9fac72626aa9afdcbe42680b
vagrant <vagrant@localhost.localdomain>
2020-09-19 06:36:48 +0000

    add_top_controller_add

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,13 +1,30 @@
 class Admins::GenresController < ApplicationController
   def index
+  	@genre = Genre.new
+  	@genres = Genre.all
   end
 
   def create
+  	@genre = Genre.new(genre_params)
+  	if @genre.save
+  	  redirect_to admins_genres_path
+    else
+      render :index
+    end
   end
 
   def edit
+  	@genre = Genre.find(params[:id])
   end
 
   def update
+  	@genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+  	redirect_to admins_genres_path
   end
+
+  protected
+    def genre_params
+      params.require(:genre).permit(:genre_name, :genre_status)
+    end
 end
