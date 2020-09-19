e71cba34d3ec76233521c62c24acfc6aec1c15a6
vagrant <vagrant@localhost.localdomain>
2020-09-19 07:12:22 +0000

    add_top

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,15 @@ class ProductsController < ApplicationController
   end
 
   def show
+  	@genres = Genre.all
+  	@product = Product.find(params[:id])
   end
 
   def about
   end
 
   def top
+  	@genres = Genre.all
+  	@recommendation = Product.all.limit(4)
   end
 end
