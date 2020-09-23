83f296fda8c744c2ffa7824772133ca515408692
vagrant <vagrant@localhost.localdomain>
2020-09-23 05:49:51 +0000

    [Fix]admin_log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,23 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :zipcode, :address, :tel])
   end
+
+# ログアウト後遷移
+  def after_sign_out_path_for(resource)
+	if resource == :admin
+		new_admin_session_path
+	else
+		root_path
+	end
+end
+	
+	# ログイン後遷移
+  def after_sign_in_path_for(resource)
+  	if resource.instance_of?(Admin)
+  		admins_admins_home_path
+  	else
+  		root_path
+  	end
+  end
+
 end
