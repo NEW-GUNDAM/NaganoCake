238501faa5448a58871cae1575a7c895cc550a6a
vagrant <vagrant@localhost.localdomain>
2020-09-23 08:26:51 +0000

    Add validates

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,19 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :tel, presence: true, format: {with: /\A\d{10,11}\z/}
+  validates :zipcode, presence: true, format: {with: /\A\d{7}\z/}
+  validates :address, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,14 @@ class Product < ApplicationRecord
   has_many :cart_items
   attachment :image
 
+  with_options presence: true do
+    validates :genre_id
+    validates :name, length: {minimum: 1}
+    validates :introduction
+    validates :price
+  end
+
+  validates :status, inclusion: { in: [true, false] }
 
     TAX = 1.1
 
