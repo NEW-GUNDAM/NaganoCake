a68a262b9222d4e09d032eae040d19b7953f3a1e
vagrant <vagrant@localhost.localdomain>
2020-09-23 10:19:51 +0000

    admin order

--- app/controllers/admins/order_datails_controller.rb
+++ app/controllers/admins/order_datails_controller.rb
@@ -1,7 +1,15 @@
 class Admins::OrderDatailsController < ApplicationController
   def update
   	@order_item = OrderItem.find(params[:id])
+  	@order = @order_item.order
   	@order_item.update(order_item_params)
+	if
+	  @order_item.make_status == "製作中"
+	  @order.update(order_status: "製作中")
+	elsif
+	  @order.order_items.count == @order.order_items.where(make_status: "製作完了").count
+	  @order.update(order_status: "発送準備中")
+	end
   	redirect_to admins_order_item_path(@order_item)
   end
 
--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -14,12 +14,20 @@ class Admins::OrderItemsController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-  	@order.update(order_params)
+    @order_items = @order.order_items
+    @order.update(order_params)
+
+      if @order.order_status == "入金待ち"
+        @order_items.update(make_status: "着手不可")
+      elsif @order.order_status == "入金確認"
+        @order_items.update(make_status: "製作待ち")
+      end
+
   	redirect_to admins_order_item_path(@order)
   end
 
   protected
   def order_params
-    params.require(:order).permit(:order_status)
+    params.require(:order).permit(:order_status, :customer_id, :addressee, :zipcode ,:send_to_address ,:freight ,:total_price ,:how_to_pay)
   end
 end
