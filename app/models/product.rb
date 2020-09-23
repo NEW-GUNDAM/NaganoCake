66abb4ddb65c395f242ad3f19e4e6fc36acf6b23
vagrant <vagrant@localhost.localdomain>
2020-09-23 05:53:36 +0000

    Fix products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,12 @@ class Product < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :cart_items
   attachment :image
+
+
+    TAX = 1.1
+
+    def tax_price
+      tax_price = price * TAX
+      tax_price.round
+    end
 end
