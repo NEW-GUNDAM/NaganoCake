4f4236d40ff0e3d100948234012f10dc85c571d1
ayana0120 <68426355+ayana0120@users.noreply.github.com>
2020-09-28 12:28:51 +0900

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :customer
 
-  validates :zipcode, format: {with: /\A\d{7}\z/}, numericality: true
+  with_options presence: true do
+	  validates :zipcode, format: {with: /\A\d{7}\z/}, numericality: true
+	  validates :ship_name
+	  validates :address
+	end
 
 
 
